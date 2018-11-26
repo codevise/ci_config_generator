6517b96213395a8823fde19a3121f2df0631bb26
Jörg Runkel <jrunkel@codevise.de>
2018-11-26 17:28:50 +0100

    Move methods in module to avoid clashes in global scope

--- lib/ci_config_generator.rb
+++ lib/ci_config_generator.rb
@@ -1,4 +1,17 @@
 require "ci_config_generator/version"
 
 module CiConfigGenerator
+
+  def self.interpolate(file_name, variables)
+    File.read(file_name).gsub(/%{[^}]+}/) do |match|
+      key = match.gsub(/^%{([^}]+)}/, '\1')
+      variables[key] || error("Cannot interpolate unknown environment variable '#{key}' in '#{file_name}'")
+    end
+  end
+
+  def self.error(message)
+    STDERR.puts("Error: #{message}")
+    exit 1
+  end
+
 end
--- lib/ci_config_generator/tasks.rb
+++ lib/ci_config_generator/tasks.rb
@@ -1,3 +1,5 @@
+require 'ci_config_generator'
+
 namespace :ci do
   namespace :config do
     desc 'Generate config files from *.ci templates'
@@ -9,21 +11,10 @@ namespace :ci do
           error "#{target_file_name} already exists."
         else
           STDERR.puts("Creating #{target_file_name} from #{file_name}")
-          File.write(target_file_name, interpolate(file_name, ENV))
+          File.write(target_file_name, CiConfigGenerator.interpolate(file_name, ENV))
         end
       end
     end
 
-    def interpolate(file_name, variables)
-      File.read(file_name).gsub(/%{[^}]+}/) do |match|
-        key = match.gsub(/^%{([^}]+)}/, '\1')
-        variables[key] || error("Cannot interpolate unknown environment variable '#{key}' in '#{file_name}'")
-      end
-    end
-
-    def error(message)
-      STDERR.puts("Error: #{message}")
-      exit 1
-    end
   end
 end
